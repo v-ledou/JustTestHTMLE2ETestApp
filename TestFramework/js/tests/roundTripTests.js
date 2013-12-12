be13ddfe7beee755383b15d753f8a7e38ec2258c
Donnic <v-ledou@microsoft.com>
2013-12-11 23:06:16 -0800

    test 12/12/2013

--- TestFramework/js/tests/roundTripTests.js
+++ TestFramework/js/tests/roundTripTests.js
@@ -27,77 +27,77 @@ function defineRoundTripTestsNamespace() {
         });
     }));
 
-    tests.push(createRoundTripTest('String: empty', 'string1', ''));
-    tests.push(createRoundTripTest('String: null', 'string1', null));
-    tests.push(createRoundTripTest('String: multiple characters', 'string1',
-        ' !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'));
-    tests.push(createRoundTripTest('String: large (1000 characters)', 'string1', Array(1001).join('*')));
-    tests.push(createRoundTripTest('String: large (64k+ characters)', 'string1', Array(65538).join('*')));
+    //tests.push(createRoundTripTest('String: empty', 'string1', ''));
+    //tests.push(createRoundTripTest('String: null', 'string1', null));
+    //tests.push(createRoundTripTest('String: multiple characters', 'string1',
+    //    ' !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'));
+    //tests.push(createRoundTripTest('String: large (1000 characters)', 'string1', Array(1001).join('*')));
+    //tests.push(createRoundTripTest('String: large (64k+ characters)', 'string1', Array(65538).join('*')));
 
-    tests.push(createRoundTripTest('String: non-ASCII characters - Latin', 'string1', 'ãéìôü ÇñÑ'));
-    tests.push(createRoundTripTest('String: non-ASCII characters - Arabic', 'string1', 'الكتاب على الطاولة'));
-    tests.push(createRoundTripTest('String: non-ASCII characters - Chinese', 'string1', '这本书在桌子上'));
-    tests.push(createRoundTripTest('String: non-ASCII characters - Chinese 2', 'string1', '⒈①Ⅻㄨㄩ 啊阿鼾齄 丂丄狚狛 狜狝﨨﨩 ˊˋ˙–〇 㐀㐁䶴䶵'));
-    tests.push(createRoundTripTest('String: non-ASCII characters - Japanese', 'string1', '本は机の上に'));
-    tests.push(createRoundTripTest('String: non-ASCII characters - Hebrew', 'string1', 'הספר הוא על השולחן'));
+    //tests.push(createRoundTripTest('String: non-ASCII characters - Latin', 'string1', 'ãéìôü ÇñÑ'));
+    //tests.push(createRoundTripTest('String: non-ASCII characters - Arabic', 'string1', 'الكتاب على الطاولة'));
+    //tests.push(createRoundTripTest('String: non-ASCII characters - Chinese', 'string1', '这本书在桌子上'));
+    //tests.push(createRoundTripTest('String: non-ASCII characters - Chinese 2', 'string1', '⒈①Ⅻㄨㄩ 啊阿鼾齄 丂丄狚狛 狜狝﨨﨩 ˊˋ˙–〇 㐀㐁䶴䶵'));
+    //tests.push(createRoundTripTest('String: non-ASCII characters - Japanese', 'string1', '本は机の上に'));
+    //tests.push(createRoundTripTest('String: non-ASCII characters - Hebrew', 'string1', 'הספר הוא על השולחן'));
 
-    tests.push(createRoundTripTest('Date: now', 'date1', new Date()));
-    tests.push(createRoundTripTest('Date: null', 'date1', null));
-    tests.push(createRoundTripTest('Date: unix 0', 'date1', new Date(Date.UTC(1970, 1 - 1, 1, 0, 0, 0, 0))));
-    tests.push(createRoundTripTest('Date: before unix 0', 'date1', new Date(Date.UTC(1969, 12 - 1, 31, 23, 59, 59, 999))));
-    tests.push(createRoundTripTest('Date: after unix 0', 'date1', new Date(Date.UTC(1970, 1 - 1, 1, 0, 0, 0, 1))));
-    tests.push(createRoundTripTest('Date: distant past', 'date1', new Date(1, 2 - 1, 3, 4, 5, 6, 7)));
-    tests.push(createRoundTripTest('Date: distant future', 'date1', new Date(9876, 5 - 1, 4, 3, 2, 1)));
+    //tests.push(createRoundTripTest('Date: now', 'date1', new Date()));
+    //tests.push(createRoundTripTest('Date: null', 'date1', null));
+    //tests.push(createRoundTripTest('Date: unix 0', 'date1', new Date(Date.UTC(1970, 1 - 1, 1, 0, 0, 0, 0))));
+    //tests.push(createRoundTripTest('Date: before unix 0', 'date1', new Date(Date.UTC(1969, 12 - 1, 31, 23, 59, 59, 999))));
+    //tests.push(createRoundTripTest('Date: after unix 0', 'date1', new Date(Date.UTC(1970, 1 - 1, 1, 0, 0, 0, 1))));
+    //tests.push(createRoundTripTest('Date: distant past', 'date1', new Date(1, 2 - 1, 3, 4, 5, 6, 7)));
+    //tests.push(createRoundTripTest('Date: distant future', 'date1', new Date(9876, 5 - 1, 4, 3, 2, 1)));
 
-    tests.push(createRoundTripTest('Bool: true', 'bool1', true));
-    tests.push(createRoundTripTest('Bool: false', 'bool1', false));
-    tests.push(createRoundTripTest('Bool: null', 'bool1', null));
+    //tests.push(createRoundTripTest('Bool: true', 'bool1', true));
+    //tests.push(createRoundTripTest('Bool: false', 'bool1', false));
+    //tests.push(createRoundTripTest('Bool: null', 'bool1', null));
 
-    tests.push(createRoundTripTest('Number: zero', 'number', 0));
-    tests.push(createRoundTripTest('Number: positive', 'number', 12345678));
-    tests.push(createRoundTripTest('Number: negative', 'number', -1232435));
-    tests.push(createRoundTripTest('Number: large long', 'number', 1152921504606846975));
-    tests.push(createRoundTripTest('Number: large long (negative)', 'number', -1152921504606846976));
-    tests.push(createRoundTripTest('Number: floating point', 'number', 12345.678));
-    tests.push(createRoundTripTest('Number: POSITIVE_INFINITY', 'number', Number.POSITIVE_INFINITY));
-    tests.push(createRoundTripTest('Number: NEGATIVE_INFINITY', 'number', Number.NEGATIVE_INFINITY));
-    tests.push(createRoundTripTest('Number: MAX_VALUE', 'number', Number.MAX_VALUE));
-    tests.push(createRoundTripTest('Number: MIN_VALUE', 'number', Number.MIN_VALUE));
-    tests.push(createRoundTripTest('Number: positive NaN', 'number', Number.NaN));
-    tests.push(createRoundTripTest('Number: large floating point', 'number', 1.234e+123));
+    //tests.push(createRoundTripTest('Number: zero', 'number', 0));
+    //tests.push(createRoundTripTest('Number: positive', 'number', 12345678));
+    //tests.push(createRoundTripTest('Number: negative', 'number', -1232435));
+    //tests.push(createRoundTripTest('Number: large long', 'number', 1152921504606846975));
+    //tests.push(createRoundTripTest('Number: large long (negative)', 'number', -1152921504606846976));
+    //tests.push(createRoundTripTest('Number: floating point', 'number', 12345.678));
+    //tests.push(createRoundTripTest('Number: POSITIVE_INFINITY', 'number', Number.POSITIVE_INFINITY));
+    //tests.push(createRoundTripTest('Number: NEGATIVE_INFINITY', 'number', Number.NEGATIVE_INFINITY));
+    //tests.push(createRoundTripTest('Number: MAX_VALUE', 'number', Number.MAX_VALUE));
+    //tests.push(createRoundTripTest('Number: MIN_VALUE', 'number', Number.MIN_VALUE));
+    //tests.push(createRoundTripTest('Number: positive NaN', 'number', Number.NaN));
+    //tests.push(createRoundTripTest('Number: large floating point', 'number', 1.234e+123));
 
-    tests.push(createRoundTripTest('Complex type (object): simple value', 'complexType',
-        { Name: 'John Doe', Age: 33, Friends: ['Jane Roe', 'John Smith'] }));
-    tests.push(createRoundTripTest('Complex type (object): null', 'complexType', null));
-    tests.push(createRoundTripTest('Complex type (object): empty object', 'complexType', {}));
-    tests.push(createRoundTripTest('Complex type (array): simple value', 'complexType',
-        [{ Name: 'Scooby Doo', Age: 10 }, { Name: 'Shaggy', Age: 19 }]));
-    tests.push(createRoundTripTest('Complex type (array): empty array', 'complexType', []));
-    tests.push(createRoundTripTest('Complex type (array): array with null elements', 'complexType',
-        [{ Name: 'Scooby Doo', Age: 10 }, null, { Name: 'Shaggy', Age: 19 }]));
+    //tests.push(createRoundTripTest('Complex type (object): simple value', 'complexType',
+    //    { Name: 'John Doe', Age: 33, Friends: ['Jane Roe', 'John Smith'] }));
+    //tests.push(createRoundTripTest('Complex type (object): null', 'complexType', null));
+    //tests.push(createRoundTripTest('Complex type (object): empty object', 'complexType', {}));
+    //tests.push(createRoundTripTest('Complex type (array): simple value', 'complexType',
+    //    [{ Name: 'Scooby Doo', Age: 10 }, { Name: 'Shaggy', Age: 19 }]));
+    //tests.push(createRoundTripTest('Complex type (array): empty array', 'complexType', []));
+    //tests.push(createRoundTripTest('Complex type (array): array with null elements', 'complexType',
+    //    [{ Name: 'Scooby Doo', Age: 10 }, null, { Name: 'Shaggy', Age: 19 }]));
 
-    tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'id\' property (value = 0)', { id: 0, string1: 'hello' }));
-    tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'id\' property (value = 1)', { id: 1, string1: 'hello' }));
-    tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'Id\' property (value = 1)', { Id: 1, string1: 'hello' }));
-    tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'ID\' property (value = 1)', { ID: 1, string1: 'hello' }));
+    //tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'id\' property (value = 0)', { id: 0, string1: 'hello' }));
+    //tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'id\' property (value = 1)', { id: 1, string1: 'hello' }));
+    //tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'Id\' property (value = 1)', { Id: 1, string1: 'hello' }));
+    //tests.push(createNegativeRoundTripTest('(Neg) Insert item with \'ID\' property (value = 1)', { ID: 1, string1: 'hello' }));
 
-    tests.push(new zumo.Test('Setup string id dynamic schema', function (test, done) {
-        var table = zumo.getClient().getTable(stringIdTableName);
-        var item = {
-            name: 'a string',
-            number: 123.45,
-            bool: true,
-            date1: new Date(),
-            complex: [ 'a complex object which will be converted to string at the database' ]
-        };
-        table.insert(item).done(function () {
-            test.addLog('Successfully set up the dynamic schema: ' + JSON.stringify(item));
-            done(true);
-        }, function (err) {
-            test.addLog('Error setting up dynamic schema: ' + JSON.stringify(err));
-            done(false);
-        });
-    }));
+    //tests.push(new zumo.Test('Setup string id dynamic schema', function (test, done) {
+    //    var table = zumo.getClient().getTable(stringIdTableName);
+    //    var item = {
+    //        name: 'a string',
+    //        number: 123.45,
+    //        bool: true,
+    //        date1: new Date(),
+    //        complex: [ 'a complex object which will be converted to string at the database' ]
+    //    };
+    //    table.insert(item).done(function () {
+    //        test.addLog('Successfully set up the dynamic schema: ' + JSON.stringify(item));
+    //        done(true);
+    //    }, function (err) {
+    //        test.addLog('Error setting up dynamic schema: ' + JSON.stringify(err));
+    //        done(false);
+    //    });
+    //}));
 
     var itemName = 'ãéìôü ÇñÑ - الكتاب على الطاولة - 这本书在桌子上 - ⒈①Ⅻㄨㄩ 啊阿鼾齄 丂丄狚狛 狜狝﨨﨩 ˊˋ˙–〇 㐀㐁䶴䶵 - 本は机の上に - הספר הוא על השולחן';
     var itemNumber = Number.MAX_VALUE;
